8ac2f5949f594567e760ec8fad6b64381952c2da
thoma <chemthunderisme@gmail.com>
2025-09-09 17:07:25 -0400

    :3

--- src/main/java/dev/chemthunder/northstar/effect/RallyEffect.java
+++ src/main/java/dev/chemthunder/northstar/effect/RallyEffect.java
@@ -1,7 +1,6 @@
 package dev.chemthunder.northstar.effect;
 
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
@@ -9,7 +8,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import org.jetbrains.annotations.Nullable;
 
 public class RallyEffect extends StatusEffect {
     public RallyEffect() {
@@ -18,13 +16,8 @@ public class RallyEffect extends StatusEffect {
 
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
-        return super.applyUpdateEffect(entity, amplifier);
-    }
 
-    @Override
-    public void applyInstantEffect(@Nullable Entity source, @Nullable Entity attacker, LivingEntity target, int amplifier, double proximity) {
-        target.heal(50);
-        super.applyInstantEffect(source, attacker, target, amplifier, proximity);
+        return super.applyUpdateEffect(entity, amplifier);
     }
 
     public ParticleEffect createParticle(StatusEffectInstance effect) {
--- src/main/java/dev/chemthunder/northstar/item/EnsculkedLumiumItem.java
+++ src/main/java/dev/chemthunder/northstar/item/EnsculkedLumiumItem.java
@@ -1,12 +1,15 @@
 package dev.chemthunder.northstar.item;
 
+import dev.chemthunder.northstar.init.NorthEffects;
 import dev.chemthunder.northstar.init.NorthItems;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 
@@ -30,7 +33,23 @@ public class EnsculkedLumiumItem extends Item {
             }
             return ActionResult.SUCCESS;
         }
+
+        if (user != null && user.isSneaking() && state.isOf(Blocks.LAVA_CAULDRON)) {
+            ItemStack stack = user.getMainHandStack();
+            ItemStack offStack = user.getOffHandStack();
+
+            if (stack.isOf(Items.NETHERITE_SWORD) && offStack.isOf(NorthItems.ENSCULKED_LUMIUM)) {
+                offStack.decrement(1);
+                stack.decrement(1);
+                user.giveItemStack(NorthItems.GRACE.getDefaultStack());
+                user.addStatusEffect(new StatusEffectInstance(NorthEffects.RALLY, 120));
+                user.playSound(SoundEvents.ITEM_TRIDENT_RETURN, 0.8F, 1.0F);
+            }
+            return ActionResult.SUCCESS;
+        }
         return super.useOnBlock(context);
     }
+
+
 
 }
